1aded9538a5e81d587d8489d211eeeac0fe8b674
Alexots <115954644+Alexots@users.noreply.github.com>
2023-08-06 14:43:00 +0300

    Update runme.py

--- runme.py
+++ runme.py
@@ -34,7 +34,7 @@ sleep_to = 20
 
 link_to_go = 'https://twitter.com/intent/follow?screen_name='
 
-button_follow_selector = str("""#layers > div:nth-child(2) > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-1kihuf0.r-18u37iz.r-1pi2tsx.r-1777fci.r-1pjcn9w.r-xr3zp9.r-1xcajam.r-ipm5af.r-9dcw1g > div.css-1dbjc4n.r-z6ln5t.r-14lw9ot.r-1867qdf.r-1jgb5lz.r-pm9dpa.r-1ye8kvj.r-1rnoaur.r-494qqr.r-13qz1uu > div.css-1dbjc4n.r-eqz5dr.r-1hc659g.r-1n2ue9f.r-11c0sde.r-13qz1uu > div.css-18t94o4.css-1dbjc4n.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-16y2uox.r-6gpygo.r-peo1c.r-1ps3wis.r-1ny4l3l.r-1udh08x.r-1guathk.r-1udbk01.r-o7ynqc.r-6416eg.r-lrvibr.r-3s2u2q > div > span > span""")
+button_follow_selector = "//*[@data-testid='confirmationSheetConfirm']"
 
 def start(profile):
     bad = []
@@ -54,7 +54,7 @@ def start(profile):
         try:
             driver.get(link_to_go+i)
             time.sleep(random.randint(15,20))
-            btn = driver.find_element(By.CSS_SELECTOR,button_follow_selector)
+            btn = driver.find_element(By.XPATH,button_follow_selector)
             btn.click()
             time.sleep(random.randint(sleep_from,sleep_to))
 
